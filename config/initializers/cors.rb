a3b65074d03fd8d664988aeaff8cb81c351ce574
Jeffrey Marks <JKDMarks@gmail.com>
2019-05-30 15:16:51 -0400

    All CORS requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://swimmy-otter.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
